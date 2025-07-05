63ca1fbef5c3cc855b6fe357370ec177cf34545a
hamidwise1 <hrawebdev@gmail.com>
2025-07-05 20:05:51 +0330

    add profile component

--- app/[lang]/(website)/components/Profile.tsx
+++ app/[lang]/(website)/components/Profile.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 export default function Profile() {
- return <div className='order-3 grow flex justify-end'>profile</div>;
+ return (
+  <div className='order-3 grow flex justify-end self-center'>
+   <Link
+    href='/'
+    className='border border-neutral-300 rounded-full text-neutral-600 dark:border-neutral-700 dark:text-neutral-400'
+   >
+    <Avatar className='size-10'>
+     {/* <AvatarImage src='https://github.com/shadcn.png' /> */}
+     <AvatarFallback>HRA</AvatarFallback>
+    </Avatar>
+   </Link>
+  </div>
+ );
 }
