229247e12705b16ddca5bdb0cee05086bfbd5595
hamidwise1 <hrawebdev@gmail.com>
2025-07-13 12:56:42 +0330

    update project overview

--- app/[lang]/(website)/components/home/ProjectsOverview.tsx
+++ app/[lang]/(website)/components/home/ProjectsOverview.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
+const timeFormatter = new Intl.DateTimeFormat('fa', {
+ dateStyle: 'long',
+});
 
 export default function ProjectsOverview() {
  return (
   <section>
    <ul>
     {Array.from({ length: 3 }, (_, i) => i).map((i) => (
-     <li key={i} className='mb-4'>
+     <li key={i} className='mb-6'>
       <Link href='#' className='block'>
+       <div className='text-neutral-600 dark:text-neutral-400 text-xs'>
+        {timeFormatter.format(new Date())}
+       </div>
        <h3 className='text-xl font-medium mb-2'>بازبینی سیستم هتل‌داری</h3>
        <p className='text-[0.8rem] leading-6 w-[min(100%,33rem)] mb-1'>
         اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید به متن های
