7ef60b9ad3b7ad2b24a8900d45fdec285b558dc5
hamidwise1 <hrawebdev@gmail.com>
2025-07-13 08:52:43 +0330

    add hero section description and socials

--- app/[lang]/(website)/components/home/Hero.tsx
+++ app/[lang]/(website)/components/home/Hero.tsx
@@ -1,4 +1,9 @@
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { FaGithub } from 'react-icons/fa6';
+import { BsTelegram } from 'react-icons/bs';
+import { IoLogoLinkedin } from 'react-icons/io5';
+
 export default function Hero() {
  return (
   <div>
@@ -10,9 +15,32 @@ export default function Hero() {
     />
    </Link>
    <div>
-    <h1 className='text-2xl lg:text-4xl font-bold'>
+    <h1 className='text-2xl lg:text-4xl font-bold mb-6'>
      طــــراح برنامه، برنامه نویس‌وب
     </h1>
+    <p className='w-[min(100%,43rem)] leading-7 text-neutral-600 dark:text-neutral-400 mb-6'>
+     اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید به متن های
+     برخورده اید که با نام لورم ایپسوم شناخته می‌شوند. لورم ایپسوم یا طرح‌نما
+     (به انگلیسی: Lorem ipsum) متنی ساختگی و بدون معنی است که برای امتحان فونت و
+     یا پر کردن فضا در یک طراحی گرافیکی و یا صنعت چاپ استفاده میشود.
+    </p>
+    <div className='flex flex-wrap gap-2 text-neutral-500 dark:text-neutral-400'>
+     <Button size='icon' variant='ghost' asChild>
+      <Link href='#'>
+       <FaGithub className='size-6' />
+      </Link>
+     </Button>
+     <Button size='icon' variant='ghost' asChild>
+      <Link href='#'>
+       <BsTelegram className='size-6' />
+      </Link>
+     </Button>
+     <Button size='icon' variant='ghost' asChild>
+      <Link href='#'>
+       <IoLogoLinkedin className='size-6' />
+      </Link>
+     </Button>
+    </div>
    </div>
   </div>
  );
