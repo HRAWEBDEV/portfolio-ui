7b53091193d2e1893be1fb8e7944e624a36f6321
hamidwise1 <hrawebdev@gmail.com>
2025-08-05 09:42:54 +0330

    update projects overview

--- app/[lang]/(website)/components/home/ProjectsOverview.tsx
+++ app/[lang]/(website)/components/home/ProjectsOverview.tsx
@@ -12,7 +12,7 @@ export default function ProjectsOverview() {
     {Array.from({ length: 3 }, (_, i) => i).map((i) => (
      <li
       key={i}
-      className='p-4 hover:bg-neutral-100 dark:hover:bg-background rounded-lg'
+      className='p-4 hover:bg-neutral-50 dark:hover:bg-background rounded-lg'
      >
       <Link href='#' className='block'>
        <div className='text-neutral-600 dark:text-neutral-400 text-xs'>
